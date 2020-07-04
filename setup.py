331a6a30bd2bf632b498300771338914814f17ec
hotkeysoft <dev@hotkeysoft.net>
2020-07-04 14:51:54 -0400

    Readme usage & setup classifiers

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="chiplabel",
-    version="1.0.0",
+    name="chiplabel-hotkeysoft",
+    version="1.0.0a1",
     author="Dominic Thibodeau",
     author_email="dev@hotkeysoft.net",
     description="Chip label generator",
@@ -16,14 +16,19 @@ setuptools.setup(
     include_package_data = True,
     package_data={'chiplabel': ['fonts/*', 'chips/*']},
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
         "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
     ],
     python_requires='>=3.6',
     install_requires=['Pillow', 'PyYAML'],
-    #scripts = ['bin/chip_label.py'],
     entry_points={
         'console_scripts': ['chip_label=chiplabel.__main__:main']
-    },    
+    },
  )
