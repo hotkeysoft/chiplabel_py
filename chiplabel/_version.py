cd2e1d01b5fb036b0ea0b2983bc595b62b7ff016
hotkeysoft <dev@hotkeysoft.net>
2021-03-22 20:19:52 -0400

    Version 1.0.1

--- chiplabel/_version.py
+++ chiplabel/_version.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # _version.py
-__version__ = '1.0.0a1'
+__version__ = '1.0.1'
 __app__ = 'chip_label'
 __author__ = 'Dominic Thibodeau'
 __author_email__ = 'dev@hotkeysoft.net'
 __description__ = 'Chip Label Generator'
 __url__ = 'https://github.com/hotkeysoft/chiplabel_py'
-__year__ = 2020
+__year__ = 2021
 
 def print_version_info():
     v = f'{__app__} {__description__} version {__version__}'
